6548cfe4d534b8c0811682528f4a9929fe33d92f
Georges Arnould <garnould@oxoxo.net>
2013-11-29 11:06:50 +0100

    Renamed feed

--- scripts/rawizer.sh
+++ scripts/rawizer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cat ArduinOPML.xml | perl -e 'while ($l = <>) { $e = ($l =~ /xmlUrl="([^"]+)"/g)[0]; print $e, "\n" if ($e) ;}' | sort -u > ArduinOPML.raw.txt
+cat ArduinOPML.opml | perl -e 'while ($l = <>) { $e = ($l =~ /xmlUrl="([^"]+)"/g)[0]; print $e, "\n" if ($e) ;}' | sort -u > ArduinOPML.raw.txt
 
-cat ArduinOPML.xml  | perl -ple 's/htmlUrl="([^"]+)"/htmlUrl="$1?utm_source=ArduinOPML&utm_campaign=Please_contribute_on_ArduinOPML&utm_medium=RSS"/g' > ArduinOPML.xml-new
+cat ArduinOPML.opml  | perl -ple 's/htmlUrl="([^"]+)"/htmlUrl="$1?utm_source=ArduinOPML&utm_campaign=Please_contribute_on_ArduinOPML&utm_medium=RSS"/g' > ArduinOPML.opml-new
 
 echo "Think about utm_ !"
