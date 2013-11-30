93720ce09ec0caf2f649ba2eb16e85b6c3de60fc
Georges Arnould <garnould@oxoxo.net>
2013-11-30 12:14:38 +0100

    ArduinOPML (http://bit.ly/pabdjv): 379 Arduino-related feeds, feat. Kevin Darrah and his 8^3 led cube (http://bit.ly/1840Gct)

--- scripts/rawizer.sh
+++ scripts/rawizer.sh
@@ -2,6 +2,7 @@
 
 cat ArduinOPML.opml | perl -e 'while ($l = <>) { $e = ($l =~ /xmlUrl="([^"]+)"/g)[0]; print $e, "\n" if ($e) ;}' | sort -u > ArduinOPML.raw.txt
 
-cat ArduinOPML.opml  | perl -ple 's/htmlUrl="([^"]+)"/htmlUrl="$1?utm_source=ArduinOPML&utm_campaign=Please_contribute_on_ArduinOPML&utm_medium=RSS"/g' > ArduinOPML.opml-new
+cat ArduinOPML.opml | perl -ple 's/htmlUrl="([^"]+)"/htmlUrl="$1?utm_source=ArduinOPML&utm_campaign=Please_contribute_on_ArduinOPML&utm_medium=RSS"/g' > ArduinOPML.opml-new
+mv -f ArduinOPML.opml-new ArduinOPML.opml
 
-echo "Think about utm_ !"
+echo "Feeds: `wc -l ArduinOPML.raw.txt`"
